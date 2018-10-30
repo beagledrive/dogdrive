d882f33ef20c8068d7c34574340b8c8aa329f3a5
Tamas <takovacs@kth.se>
2018-10-30 12:55:48 +0100

    Added coding template for dsp

---
+++ tempfunc.c
@@ -0,0 +1,21 @@
+/*
+ * Beaglebone Open-Source Machine Drive
+ * Template for source file for definitions in the header file
+ * KTH Project Work - 2018 
+ */
+
+
+/* ================================== INCLUDES ============================== */
+
+#include <template.h>
+
+
+/* ================================== FUNCTION DEFINITIONS ================== */
+
+
+void recruit_legionnaire(int *num_of_units)
+{
+	*num_of_units += 1;
+
+	return;	
+};
---
+++ template.h
@@ -0,0 +1,23 @@
+/*
+ * Beaglebone Open-Source Machine Drive
+ * C Header template
+ * KTH Project Work - 2018 
+ */
+
+#ifndef TEMPLATE_H_		// Header wrapper name
+#define TEMPLATE_H_
+
+/*
+ * What to include in own header file:
+ * 	Definitions of data types
+ * 	Funtion prototypes
+ * 	C preprocessor commands
+ * Use only if the above are needed in multiple source files, otherwise leave header file empty.
+ */
+
+/*
+ * This is just a simple increment funtion for illustrative purposes only
+ */
+void recruit_legionnaire(int *num_of_units);
+
+#endif
