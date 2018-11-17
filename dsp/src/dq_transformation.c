927e39ec031862594ca9e68c9b2cf7cdf5d02dd7
Abhishek Maji <43369573+AMaji1@users.noreply.github.com>
2018-11-17 12:42:51 +0100

    Add files via upload

---
+++ dsp/src/dq_transformation.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*******  d-q transformation ******/
+
+typedef struct
+{
+    float K;                  // space-vector scaling constant
+
+}DQ_Typedef;
+
+void DQ_StructInit(DQ_Typedef *DQ_Struct,float SpcVectScaling);
+void DQ_Transformation(DQ_Typedef *DQ_Struct,float Ia,float Ib,float Ic,float theta1,float *Id,float *Iq);
+
+void DQ_StructInit(DQ_Typedef *DQ_Struct,float SpcVectScaling)
+{
+    DQ_Struct->K = SpcVectScaling;
+
+}
+void DQ_Transformation(DQ_Typedef *DQ_Struct,float Ia,float Ib,float Ic,float theta1,float *Id,float *Iq)
+{
+    float Ialpha = 0;
+    float Ibeta = 0;
+    float sin_theta1 = 0;
+    float cos_theta1 = 0;
+
+    // a-b-c to alpha-beta transformation
+
+    Ialpha = DQ_Struct->K*(2*Ia - Ib - Ic)/3;
+
+    Ibeta = DQ_Struct->K*(Ib - Ic)/sqrt(3);
+
+    // alpha-beta to d-q transformation
+
+    sin_theta1 = cal_sin(theta1);
+    cos_theta1 = cal_cos(theta1);
+
+    *Id = cos_theta1*Ialpha + sin_theta1*Ibeta;
+    *Iq = -sin_theta1*Ialpha + cos_theta1*Ibeta;
+}
---
+++ dsp/src/inversedq_transformation.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*******  inverse d-q transformation ******/
+
+typedef struct
+{
+    float K;                  // space-vector scaling constant
+
+}IDQ_Typedef;
+
+void IDQ_StructInit(IDQ_Typedef *IDQ_Struct,float SpcVectScaling);
+void IDQ_Transformation(IDQ_Typedef *IDQ_Struct,float Vd,float Vq,float theta1,float *Valpha,float *Vbeta,float *Va,float *Vb,float *Vc);
+
+void IDQ_StructInit(IDQ_Typedef *IDQ_Struct,float SpcVectScaling)
+{
+    IDQ_Struct->K = SpcVectScaling;
+
+}
+void IDQ_Transformation(IDQ_Typedef *IDQ_Struct,float Vd,float Vq,float theta1,float *Valpha,float *Vbeta,float *Va,float *Vb,float *Vc)
+{
+    float sin_theta1 = 0;
+    float cos_theta1 = 0;
+
+    // d-q to alpha-beta transformation
+
+    sin_theta1 = cal_sin(theta1);
+    cos_theta1 = cal_cos(theta1);
+
+    *Valpha = cos_theta1*Vd -sin_theta1*Vq;
+
+    *Vbeta = sin_theta1*Vd + cos_theta1*Vq;
+
+    // alpha-beta to a-b-c transformation
+
+    *Ia = *Valpha/DQ_Struct->K;
+
+    *Ib = (-(*Valpha) + sqrt(3)*(*Vbeta))/(2*IDQ_Struct->K);
+
+    *Ic = (-(*Valpha) - sqrt(3)*(*Vbeta))/(2*IDQ_Struct->K);
+}
+
